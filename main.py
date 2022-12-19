1056fd7ac57a9dee6ca68c29f272fe071c543f1e
coufaan21 <coufaan21@sps-prosek.cz>
2022-12-19 17:09:00 +0100

    hototov

--- main.py
+++ main.py
@@ -13,35 +13,55 @@ with open("slova1.txt", "r") as f:
     slova1 = f.readlines()
 
 while hra:
-    # slova = ("limetka", "piano", "prsten", "smrt", "vyvrcholeni")
     znak = "-"
     zivoty = 6
+    pokus = 0
+    spravny_pokus = 0
+    uspesnost = 0
+    spatne = []
     nahodne_slovo = random.choice(slova1).strip("\n")
     rozdeleni = list(nahodne_slovo)
     # print(rozdeleni)
-    print("vase slovo ma", len(rozdeleni), "pismen")
-    znaky = list(znak * len(rozdeleni))
+    print("Tvoje slovo má", len(rozdeleni), "písmen")
+    znaky = list(znak * len(rozdeleni))  # nahradi hadane slovo -
     hadani = True
+
     while hadani:
         print(znaky)
-        zadano = input("hadej pismeno:")
+        zadano = input("hádej písmeno:")
         zadano = zadano.lower()
+
         if zadano.isalpha() and len(zadano) > 0:
+            pokus += 1
             zadano = zadano[0]
-            if zadano in rozdeleni:
-                print("zadal jsi", zadano, "a to je spravne, hadej dal:", "zbyvajici zivoty", zivoty)
+
+            if zadano in rozdeleni:  # kdyz uhodnuto spravne pismeno
+                spravny_pokus += 1
+                print("zadal jsi", zadano, "a to je správně, hádej dál:", "zbývající životy:", zivoty, "\n")
+                print("špatně zadaná písmena :", spatne)
+
                 for i in range(len(nahodne_slovo)):
                     if nahodne_slovo[i] == zadano:
                         znaky[i] = zadano
                         rozdeleni.remove(zadano)
-            else:
+            else:  # kdyz uhodnuto spatne pismeno
+                if zadano not in spatne:
+                    spatne.append(zadano)
+                else:
+                    print("to už jsi zkoušel, -1 život")
                 zivoty -= 1
-                print("pismeno", zadano, "neni ve slove, hadej dal:", "zbyvajici zivoty", zivoty)
-            if not rozdeleni:
-                print("vyhral jsi! hraj znova!\n")
+                print("písmeno", zadano, "není ve slově, hádej dál:", "zbývající životy:", zivoty, "\n")
+                print("špatně zadaná písmena :", spatne)
+
+            if not rozdeleni:  # pokud uhadnuta vsechna pismena
+                uspesnost = spravny_pokus/pokus*100
+                print("Vyhrál jsi! hraj znova! tvůj celkový počet pokusů byl", pokus,
+                      "a míra úspěšnosti=", uspesnost, "\n")
                 hadani = False
                 continue
-        if zivoty == 0:
-            print("Prohral jsi :). hraj znova!\n")
+
+        if zivoty == 0:  # pokud nezbyvaji zadne zivoty
+            print("Prohrál jsi :). hraj znova! tvůj celkový počet pokusů byl", pokus,
+                  "a míra úspěšnosti=", uspesnost,  "\n")
             hadani = False
             continue
